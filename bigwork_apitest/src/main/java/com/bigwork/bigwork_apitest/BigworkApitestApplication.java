28c0a133e058fd6e3b8ef253353570f77d01e307
Chen Fang <122147697+Monet12345@users.noreply.github.com>
2024-12-16 23:57:30 +0800

    Add files via upload (#1)

---
+++ bigwork_apitest/src/main/java/com/bigwork/bigwork_apitest/BigworkApitestApplication.java
@@ -0,0 +1,12 @@
+package com.bigwork.bigwork_apitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigworkApitestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigworkApitestApplication.class, args);
+    }
+}
