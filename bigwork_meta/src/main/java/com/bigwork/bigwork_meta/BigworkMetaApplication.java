fb93b5747da3919613df7c0af289c9606bc08e3d
Chen Fang <122147697+Monet12345@users.noreply.github.com>
2024-12-16 23:56:52 +0800

    Add files via upload

---
+++ bigwork_meta/src/main/java/com/bigwork/bigwork_meta/BigworkMetaApplication.java
@@ -0,0 +1,13 @@
+package com.bigwork.bigwork_meta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigworkMetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigworkMetaApplication.class, args);
+    }
+
+}
