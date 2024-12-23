95c063486bbaff4c73fcb9c58a5ab4071849fb9f
FC <1876476367@qq.com>
2024-12-23 09:42:46 +0800

    登录接口构想中

--- bigwork_meta/src/main/java/com/bigwork/bigwork_meta/login/SaTokenDemoApplication.java
+++ bigwork_meta/src/main/java/com/bigwork/bigwork_meta/login/SaTokenDemoApplication.java
@@ -1,6 +1,7 @@
 package com.bigwork.bigwork_meta.login;
 
 import cn.dev33.satoken.SaTokenManager;
+import cn.dev33.satoken.stp.StpUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SaTokenDemoApplication {
   public static void main(String[] args) throws JsonProcessingException {
     SpringApplication.run(SaTokenDemoApplication.class, args);
+
     System.out.println("启动成功：sa-token配置如下：" + SaTokenManager.getConfig());
+    StpUtil.isLogin();
   }
 }
