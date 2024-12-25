b35f9b50e7b28082304003d379bef560261acc25
FC <1876476367@qq.com>
2024-12-25 18:12:32 +0800

    重启项目的根

---
+++ bigwork_meta/src/main/java/com/bigwork/bigwork_meta/web/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.bigwork.bigwork_meta.web.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+  @GetMapping("/")
+  public String test() {
+    System.out.println("testsssssssssssssssssssssssssssssssssssssssssssssssssssssss");
+    return "hello world";
+  }
+
+}
