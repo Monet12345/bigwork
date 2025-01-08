001b7e13912e3496ae858c03e8fd9ccc2073ffaa
FC <1876476367@qq.com>
2025-01-08 10:33:19 +0800

    修复三方登录相关问题，加入token返回值给前端

--- new.sql
+++ new.sql
@@ -1,4 +1,4 @@
-CREATE TABLE User_db (
+CREATE TABLE user_db (
                       id INT AUTO_INCREMENT PRIMARY KEY,
                       user_id VARCHAR(255) NOT NULL UNIQUE,
                       password VARCHAR(255) NOT NULL,
