ba99d56bcc8d238fcaaf908c2eff8a5706b16818
Monet12345 <1876476367@qq.com>
2025-01-12 18:10:13 +0800

    用户登录后自动连接websocket，登出时断开，接入决策模式，暴露websocket主动发消息，收消息方法

--- bigwork_apitest/src/main/java/com/bigwork/bigwork_apitest/web/ChatController.java
+++ bigwork_apitest/src/main/java/com/bigwork/bigwork_apitest/web/ChatController.java
@@ -2,21 +2,25 @@ package com.bigwork.bigwork_apitest.web;
 
 import com.bigwork.bigwork_apitest.model.FriendListReq;
 import com.bigwork.bigwork_apitest.model.FriendVo;
+import com.bigwork.bigwork_apitest.service.ChatService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import model.Page;
 import model.Result;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping("/chat")
 @Api(value = "聊天相关接口")
 public class ChatController {
+  @Resource private ChatService chatService;
   @PostMapping("/friendList")
   @ApiOperation(value = "好友列表")
   public Result<Page<FriendVo>> friendList(@RequestBody FriendListReq friendListReq) {
 
-    return Result.buildSuccess();
+    return Result.buildSuccess(chatService.getFriendList(friendListReq));
   }
 
 }
