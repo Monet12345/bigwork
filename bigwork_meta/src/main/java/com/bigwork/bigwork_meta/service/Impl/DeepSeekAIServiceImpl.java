da5bfde0f62da0baaffd959d8d806f4a3149d03c
Monet12345 <1876476367@qq.com>
2025-01-16 21:45:39 +0800

    配了docker，待完成

--- bigwork_meta/src/main/java/com/bigwork/bigwork_meta/service/Impl/DeepSeekAIServiceImpl.java
+++ bigwork_meta/src/main/java/com/bigwork/bigwork_meta/service/Impl/DeepSeekAIServiceImpl.java
@@ -9,6 +9,7 @@ import com.bigwork.bigwork_meta.model.DeepSeekReq;
 import com.bigwork.bigwork_meta.service.DeepSeekAIService;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
